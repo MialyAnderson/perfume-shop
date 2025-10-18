c5cf32d2f7cd752a3458a5bcd239fbe2ed6eb46b
Anderson <sonic1805@Sonic1805.localdomain>
2025-10-17 23:57:17 -0400

    modification

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ import os
 # ========================================
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'votre-cle-secrete-super-longue-123456789'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///perfume_shop.db'
+import os
+
+# Configuration base de données
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///perfume_shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
